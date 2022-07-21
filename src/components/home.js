5dd16877959db78d4eb35f387b74bef76f7a5372
hosssu <hosssu@gmail.com>
2022-07-21 14:14:55 +0300

    tidy

--- src/components/home.js
+++ src/components/home.js
@@ -6,23 +6,23 @@ import CrudGet from './CrudGet';
 
 
 const home = () => {
-    const poster_URL = `https://image.tmdb.org/t/p/original`;
+
     return (
         <div>
-            <img className='mymovies' src={MymoviesPic} />
+            <img className='mymovies' src={MymoviesPic} alt="MyMovies App" />
 
             <div className='ui raised very padded text container segment' style={{ marginTop: '50px', backgroundColor: 'lightgray' }}>
 
                 <div className='LastWatched'>
                     <h2 className='ui header' >Recently watched</h2>
                     <CrudGet />
-                    <h3><a className='more'>Näytä lisää...</a></h3>
+                    <h3><a className='more'>Show more...</a></h3>
                 </div><br></br><br></br>
 
                 <div className='LastWatched'>
                     <h2 className='ui header'>Recently watched by friends</h2>
 
-                    <div className='LastWatched' style={{ padding: '0px', }}><h3><a className='more'>Näytä lisää...</a></h3>
+                    <div className='LastWatched' style={{ padding: '0px', }}><h3><a className='more'>Show More...</a></h3>
                     </div></div><br></br><br></br>
 
 
